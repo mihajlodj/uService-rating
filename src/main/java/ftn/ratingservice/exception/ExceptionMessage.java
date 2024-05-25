c5163c6ec55788abe142633478ceb6ce3b7f6c14
VukV <vuk.vukovic.2000@gmail.com>
2024-05-25 17:22:08 +0200

    feat: custom exception handling

---
+++ src/main/java/ftn/ratingservice/exception/ExceptionMessage.java
@@ -0,0 +1,12 @@
+package ftn.ratingservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionMessage {
+
+    private String message;
+
+}

286391cd0fba23822f4a9627f99260818604e5e0
Vuk Vukovic <vuk.vukovic.2000@gmail.com>
2024-05-29 23:33:51 +0200

    feat: test containers (#4)
    This sets up testcontainers for mongo

---
+++ src/test-integration/java/ftn/ratingservice/MongoIntegrationTest.java
@@ -0,0 +1,21 @@
+package ftn.ratingservice;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class MongoIntegrationTest {
+
+    @Container
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.6")
+            .withExposedPorts(27017);
+
+    @DynamicPropertySource
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+}
